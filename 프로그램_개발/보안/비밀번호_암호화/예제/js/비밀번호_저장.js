a27a327f654508037ba9d33ba88c98a5185e163b
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-04-07 16:48:50 +0900

    비밀번호 암호화 내용 추가
    bcript에 대한 내용 추가

--- 프로그램_개발/보안/비밀번호_암호화/예제/js/비밀번호_저장.js
+++ 프로그램_개발/보안/비밀번호_암호화/예제/js/비밀번호_저장.js
@@ -9,6 +9,7 @@ if (!password) {
   process.exit(0);
 }
 
+// cost와 saltRounds는 동치입니다.
 bcrypt.hash(password, saltRounds).then((hash) => {
   fs.writeFileSync(__dirname + '/비밀번호', hash);
   console.log('비밀번호가 저장되었습니다.');
